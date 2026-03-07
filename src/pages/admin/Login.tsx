fbec582f174ad8d226252eeb1b7391162831d6b2
Magnum Johanson de Abreu <magnumjabreuu@gmail.com>
2026-03-06 20:38:54 -0400

    uodate da tela de login na estilizacao

--- src/pages/admin/Login.tsx
+++ src/pages/admin/Login.tsx
@@ -1,31 +1,34 @@
-import {FaUser, FaLock} from 'react-icons/fa'
+import { FaUser, FaLock } from 'react-icons/fa'
 import { useState } from 'react'
 
 import '../../Styles/Login.css'
 
 export default function Login(): React.ReactElement {
-    const[username, setUsername] = useState('')
-    const[password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault()
 
-        console.log("teste", username, password)
-        console.log("Envio")
+        alert("Envando os dados: " + username + " - " + password)
     }
 
     return (
-        <div style={{ padding: 24 }}>
+        <div className="Login" style={{ padding: 24 }}>
+            <div className="login-container">
                 <form className="login-form" onSubmit={handleSubmit}>
-                    <h1>Acesse o sistema de Doação do CAPS</h1>
+                    <div className='login-titulo'>
+                        <h1>Sistema de Doação do CAPS</h1>
+                    </div>
                     <div className="field">
-                        <input type="email" placeholder="E-mail" 
-                        onChange={(e) => setUsername(e.target.value)} />
+                        <input type="email" placeholder="E-mail"
+
+                            onChange={(e) => setUsername(e.target.value)} />
                         <FaUser className="icon" aria-hidden />
                     </div>
                     <div className="field">
-                        <input type="password" placeholder="Senha" 
-                        onChange={(e) => setPassword(e.target.value)} />
+                        <input type="password" placeholder="Senha"
+                            onChange={(e) => setPassword(e.target.value)} />
                         <FaLock className="icon" aria-hidden />
                     </div>
 
@@ -37,9 +40,9 @@ export default function Login(): React.ReactElement {
                         <a href="#"> Esqueci minha senha</a>
 
                     </div>
-
-                    <button type="submit">Entrar</button>
-
+                    <div className='login-button'>
+                        <button type="submit">Entrar</button>
+                    </div>
                     <div className="signup-link">
                         <p>
                             Não tem uma conta? <a href="#">Cadastre-se</a>
@@ -47,5 +50,6 @@ export default function Login(): React.ReactElement {
                     </div>
                 </form>
             </div>
+        </div>
     )
 }
