9ad2f23b0c01d334bc49120f7c4ed3a8b1d66640
Magnumja <162545876+Magnumja@users.noreply.github.com>
2026-04-30 10:11:10 -0400

    Allow Render preview host

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['centro-doacao-caps.onrender.com'],
+  },
   server: {
     proxy: {
       '/api': {
